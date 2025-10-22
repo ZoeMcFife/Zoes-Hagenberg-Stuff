db085bc330b249d368557b3e20a64938f0da01aa
Zoe <93198038+ZoeMcFife@users.noreply.github.com>
2025-10-22 16:21:18 +0200

    Update FloatTypes.java

--- Modules/Semester_1/Modern Code/Übungen/2025-22-10_Übung_2/ue2_variables/src/FloatTypes.java
+++ Modules/Semester_1/Modern Code/Übungen/2025-22-10_Übung_2/ue2_variables/src/FloatTypes.java
@@ -10,5 +10,12 @@ public class FloatTypes
         System.out.println("Float value: " + price);
         System.out.println("Double value (pi): " + pi);
         System.out.println("Double value (scientific notation): " + scientificNotation);
+
+        // compare precision between float and double, divide 1 by 3 and print
+        float floatPrecision = 1.0F / 3.0F;
+        double doublePrecision = 1.0 / 3.0;
+        System.out.println("Float precision (1/3): " + floatPrecision);
+        System.out.println("Double precision (1/3): " + doublePrecision);
+
     }
 }
