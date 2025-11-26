ca174c076a6d9d9149a36f51fdff08d60cdd6f00
Zoe <93198038+ZoeMcFife@users.noreply.github.com>
2025-11-26 19:58:36 +0100

    Update FuzzySearchResult.java

--- Modules/Semester_1/Modern Code/Aufgaben/04_Arrays_and_Algorithms/Excercise4_ArraysAndAlgorithms/src/excercises/FuzzySearchResult.java
+++ Modules/Semester_1/Modern Code/Aufgaben/04_Arrays_and_Algorithms/Excercise4_ArraysAndAlgorithms/src/excercises/FuzzySearchResult.java
@@ -1,5 +1,12 @@
 package excercises;
 
+/**
+ * Record to hold the result of a fuzzy search operation.
+ *
+ * @param foundExactMatch Indicates whether an exact match was found.
+ * @param bestMatchIndex  The index of the best matching value in the array.
+ * @param bestMatchValue  The value of the best match.
+ */
 public record FuzzySearchResult(boolean foundExactMatch, int bestMatchIndex, int bestMatchValue)
 {
 
